6beaae187680a82c164731d6a139e5027b101690
Wanchalerm S <tickstudiu@gmail.com>
2019-12-27 01:21:10 +0700

    create index page

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,339 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from "react";
+import Head from "next/head";
 
 const Home = () => (
   <div>
     <Head>
       <title>Home</title>
-      <link rel='icon' href='/favicon.ico' />
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
+      <script
+        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"
+      ></script>
     </Head>
 
-    <Nav />
-
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className='row'>
-        <a href='https://nextjs.org/docs' className='card'>
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          Start Bootstrap
         </a>
-        <a href='https://nextjs.org/learn' className='card'>
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href='https://github.com/zeit/next.js/tree/master/examples'
-          className='card'
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home
+                <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-lg-3">
+          <h1 className="my-4">Shop Name</h1>
+          <div className="list-group">
+            <a href="#" className="list-group-item">
+              Category 1
+            </a>
+            <a href="#" className="list-group-item">
+              Category 2
+            </a>
+            <a href="#" className="list-group-item">
+              Category 3
+            </a>
+          </div>
+        </div>
+
+        <div className="col-lg-9">
+          <div
+            id="carouselExampleIndicators"
+            className="carousel slide my-4"
+            data-ride="carousel"
+          >
+            <ol className="carousel-indicators">
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="0"
+                className="active"
+              ></li>
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="1"
+              ></li>
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="2"
+              ></li>
+            </ol>
+            <div className="carousel-inner" role="listbox">
+              <div className="carousel-item active">
+                <img
+                  className="d-block img-fluid"
+                  src="http://placehold.it/900x350"
+                  alt="First slide"
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  className="d-block img-fluid"
+                  src="http://placehold.it/900x350"
+                  alt="Second slide"
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  className="d-block img-fluid"
+                  src="http://placehold.it/900x350"
+                  alt="Third slide"
+                />
+              </div>
+            </div>
+            <a
+              className="carousel-control-prev"
+              href="#carouselExampleIndicators"
+              role="button"
+              data-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Previous</span>
+            </a>
+            <a
+              className="carousel-control-next"
+              href="#carouselExampleIndicators"
+              role="button"
+              data-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Next</span>
+            </a>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-4 col-md-6 mb-4">
+              <div className="card h-100">
+                <a href="#">
+                  <img
+                    className="card-img-top"
+                    src="http://placehold.it/700x400"
+                    alt=""
+                  />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a href="#">Item One</a>
+                  </h4>
+                  <h5>$24.99</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Amet numquam aspernatur!
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">
+                    &#9733; &#9733; &#9733; &#9733; &#9734;
+                  </small>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 mb-4">
+              <div className="card h-100">
+                <a href="#">
+                  <img
+                    className="card-img-top"
+                    src="http://placehold.it/700x400"
+                    alt=""
+                  />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a href="#">Item Two</a>
+                  </h4>
+                  <h5>$24.99</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Amet numquam aspernatur! Lorem ipsum dolor sit amet.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">
+                    &#9733; &#9733; &#9733; &#9733; &#9734;
+                  </small>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 mb-4">
+              <div className="card h-100">
+                <a href="#">
+                  <img
+                    className="card-img-top"
+                    src="http://placehold.it/700x400"
+                    alt=""
+                  />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a href="#">Item Three</a>
+                  </h4>
+                  <h5>$24.99</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Amet numquam aspernatur!
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">
+                    &#9733; &#9733; &#9733; &#9733; &#9734;
+                  </small>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 mb-4">
+              <div className="card h-100">
+                <a href="#">
+                  <img
+                    className="card-img-top"
+                    src="http://placehold.it/700x400"
+                    alt=""
+                  />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a href="#">Item Four</a>
+                  </h4>
+                  <h5>$24.99</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Amet numquam aspernatur!
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">
+                    &#9733; &#9733; &#9733; &#9733; &#9734;
+                  </small>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 mb-4">
+              <div className="card h-100">
+                <a href="#">
+                  <img
+                    className="card-img-top"
+                    src="http://placehold.it/700x400"
+                    alt=""
+                  />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a href="#">Item Five</a>
+                  </h4>
+                  <h5>$24.99</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Amet numquam aspernatur! Lorem ipsum dolor sit amet.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">
+                    &#9733; &#9733; &#9733; &#9733; &#9734;
+                  </small>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 mb-4">
+              <div className="card h-100">
+                <a href="#">
+                  <img
+                    className="card-img-top"
+                    src="http://placehold.it/700x400"
+                    alt=""
+                  />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a href="#">Item Six</a>
+                  </h4>
+                  <h5>$24.99</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Amet numquam aspernatur!
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">
+                    &#9733; &#9733; &#9733; &#9733; &#9734;
+                  </small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
+    <footer className="py-5 bg-dark">
+      <div className="container">
+        <p className="m-0 text-center text-white">
+          Copyright &copy; Your Website 2019
+        </p>
+      </div>
+    </footer>
   </div>
-)
+);
 
-export default Home
+export default Home;
