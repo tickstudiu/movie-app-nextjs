370c5208707393553fed83d85a59168b65784e74
Wanchalerm S <tickstudiu@gmail.com>
2019-12-27 10:50:27 +0700

    Functional component State

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 const sideMenu = () => {
+  const [count, setCount] = useState(0);
+
+  // const stateArray = useState(0)
+  // const count = stateArray[0]
+  // const setCount = stateArray[1]
+
+  const increment = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+
+  const decrement = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+  };
   return (
     <div>
       <h1 className="my-4">Shop Name</h1>
@@ -13,8 +30,18 @@ const sideMenu = () => {
           Category 3
         </a>
       </div>
+      <div>
+        <button onClick={increment} className="btn btn-primary">
+          Increment Number
+        </button>
+        <button onClick={decrement} className="btn btn-primary">
+          Decrement Number
+        </button>
+
+        <h1>{count}</h1>
+      </div>
     </div>
   );
 };
 
-export default sideMenu
+export default sideMenu;
